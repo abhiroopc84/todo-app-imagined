0cd7da13aafdef3cd5573104a6e7821e30e5a6d9
abhiroopc84 <abhiroopc84@icloud.com>
2024-12-05 19:33:24 +0530

    Create Zustand todo store

--- src/state/todoStore.ts
+++ src/state/todoStore.ts
@@ -1,3 +1,6 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
 interface Todo {
   id: string;
   text: string;
@@ -8,8 +11,37 @@ interface Todo {
 
 interface TodoStore {
   todos: Todo[];
-  addTodo: (todo: Omit<Todo, 'id'>) => void;
+  addTodo: (todo: Omit<Todo, "id">) => void;
   toggleTodo: (id: string) => void;
   editTodo: (id: string, updated: Partial<Todo>) => void;
   deleteTodo: (id: string) => void;
 }
+
+export const useTodoStore = create<TodoStore>()(
+  persist(
+    (set) => ({
+      todos: [],
+      addTodo: (todo) =>
+        set((state) => ({
+          todos: [...state.todos, { id: crypto.randomUUID(), ...todo }],
+        })),
+      toggleTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+          ),
+        })),
+      editTodo: (id, updated) =>
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, ...updated } : todo
+          ),
+        })),
+      deleteTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
+    }),
+    { name: "todo-storage" }
+  )
+);
