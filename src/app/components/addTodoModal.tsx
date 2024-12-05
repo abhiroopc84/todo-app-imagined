85c59479f9702657b49c0a63b1944e95499f8c2a
abhiroopc84 <abhiroopc84@icloud.com>
2024-12-06 00:43:22 +0530

    Style changes for AddTodoModal

--- src/app/components/addTodoModal.tsx
+++ src/app/components/addTodoModal.tsx
@@ -2,12 +2,16 @@
 
 import React, { useState } from "react";
 import { useTodoStore } from "@/state/todoStore";
+import { useDateStore } from "@/state/dateStore";
+import { PlusIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
 
-const AddTodoModal: React.FC = () => {
+const AddTodoModal = () => {
   const [text, setText] = useState("");
   const [description, setDescription] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const addTodo = useTodoStore((state) => state.addTodo);
+  const { selectedDate } = useDateStore();
 
   const handleSubmit = () => {
     if (text) {
@@ -15,7 +19,7 @@ const AddTodoModal: React.FC = () => {
         text,
         description,
         completed: false,
-        date: new Date().toISOString().split("T")[0],
+        date: format(selectedDate, "yyyy-MM-dd"),
       });
       setText("");
       setDescription("");
@@ -25,39 +29,41 @@ const AddTodoModal: React.FC = () => {
 
   return (
     <>
-      <button
-        className="fixed bottom-4 right-4 bg-blue-500 text-white rounded-full p-4"
-        onClick={() => setIsOpen(true)}
-      >
-        +
-      </button>
+      <div className="fixed bottom-6 right-0 left-0 flex flex-row items-center justify-center">
+        <button
+          className="rounded-full p-4 bg-white shadow-2xl shadow-gray-950"
+          onClick={() => setIsOpen(true)}
+        >
+          <PlusIcon className="h-6 w-6" />
+        </button>
+      </div>
       {isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-4 rounded-lg">
+        <div className="fixed z-50 inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center p-4">
+          <div className="bg-white p-4 rounded-lg shadow-2xl shadow-gray-500 flex flex-col gap-4 w-full">
             <h2 className="text-lg font-bold">Add Todo</h2>
             <input
               type="text"
               placeholder="Task"
               value={text}
               onChange={(e) => setText(e.target.value)}
-              className="border p-2 rounded w-full mt-2"
+              className="border p-2 w-full rounded-md"
             />
             <textarea
               placeholder="Description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="border p-2 rounded w-full mt-2"
+              className="border p-2 w-full rounded-md"
             />
-            <div className="flex justify-end gap-2 mt-4">
+            <div className="flex justify-end gap-2">
               <button
                 onClick={() => setIsOpen(false)}
-                className="text-gray-500 hover:underline"
+                className="bg-gray-200 px-4 py-2 rounded-md"
               >
                 Cancel
               </button>
               <button
                 onClick={handleSubmit}
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="bg-black text-white px-4 py-2 rounded-md"
               >
                 Add
               </button>
