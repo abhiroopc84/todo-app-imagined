202a4004eaa475d2b312a2c697ca6bf9ed0d0f1b
abhiroopc84 <abhiroopc84@icloud.com>
2024-12-06 00:52:12 +0530

    Add header, todo, addtodomodal to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,50 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState } from "react";
+import { Todo, useTodoStore } from "@/state/todoStore";
+import TodoItem from "@/app/components/todoItem";
+import AddTodoModal from "@/app/components/addTodoModal";
+import Header from "@/app/components/header";
+import { useDateStore } from "@/state/dateStore";
+import { format, isToday, isTomorrow, isYesterday } from "date-fns";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { todos } = useTodoStore();
+  const { selectedDate } = useDateStore();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const selectedDay = format(selectedDate, "yyyy-MM-dd");
+  const filteredTodos = todos.filter((todo) => todo.date === selectedDay);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const getDateLabel = () => {
+    if (isToday(selectedDate)) {
+      return "Today";
+    }
+    if (isTomorrow(selectedDate)) {
+      return "Tomorrow";
+    }
+    if (isYesterday(selectedDate)) {
+      return "Yesterday";
+    }
+    return format(selectedDate, "MMMM d");
+  };
+
+  return (
+    <main className="h-dvh w-dvw font-sans flex flex-col justify-between">
+      <Header />
+      <section
+        className="pt-[166px] px-4 pb-24 overflow-y-scroll"
+        style={{
+          mask: "linear-gradient(to bottom, rgba(0,0,0, 1) 0, rgba(0,0,0, 1) 85%, rgba(0,0,0, 0) 90%, rgba(0,0,0, 0) 0) 100% 50% / 100% 100% repeat-x",
+        }}
+      >
+        <div className="flex flex-col gap-4">
+          <h1 className="text-xl font-bold pt-2">{getDateLabel()}</h1>
+          {filteredTodos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} onEditClick={openEditModal} />
+          ))}
         </div>
+      </section>
+      <AddTodoModal />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
